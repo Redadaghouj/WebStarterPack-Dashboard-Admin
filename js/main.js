bf4de2303b10291a15891aca1e896ff571450e8d
Mohamed Reda Daghouj <mohamedredadaghouj@gmail.com>
2023-02-25 22:45:08 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@ let navAvatar = document.querySelector(".navbar>div:last-of-type .avatar");
 let notificationBtn = document.querySelector(
   ".navbar>div:last-of-type .notification"
 );
-
 let menuTogglerBtn = document.querySelector(".navbar .menu-toggler");
-
-navAvatar.addEventListener("click", () => {
-  navAvatar.lastElementChild.classList.toggle("active");
-});
-
-notificationBtn.addEventListener("click", () => {
-  notificationBtn.lastElementChild.classList.toggle("active");
-});
 
 menuTogglerBtn.addEventListener("click", () => {
   let sidebar = document.querySelector(".content .sidebar");
   sidebar.classList.toggle("active");
 });
+
+function dropMenu(selector) {
+  selector.addEventListener("click", () => {
+    selector.lastElementChild.classList.toggle("active");
+  });
+}
+
+dropMenu(navAvatar);
+dropMenu(notificationBtn);
